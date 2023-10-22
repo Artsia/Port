be788d2addcec571ac4bf396792b3ee9595a58a3
Phillip Kataswa <tomphillip756@gmail.com>
2023-10-22 05:53:56 -0400

    complete proj.js

---
+++ app/src/comp/proj.js
@@ -0,0 +1,23 @@
+import './proj.css';
+/**Recieve
+ * @imageSrc - image location of image
+ * @liveLink -  location of live project
+ * @githubLink -  location of repo
+ */
+function Project({ imageSrc, liveLink, githubLink }) {
+  return (
+    <div className="project">
+      <img src={imageSrc} alt="Project" className="projImage" />
+      <div className="project-links">
+        <a href={liveLink} target="_blank" rel="noopener noreferrer">
+          Live
+        </a>
+        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
